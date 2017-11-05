8a945d6aadb5b0a01c9e8df7d24d2e808d89828c
Peter Kurfer <peter.kurfer@googlemail.com>
2017-11-05 22:58:51 +0100

    Added infinite sleep when attacker has completed

--- JerichoGatling/2.3.0-attacker/attack.sh
+++ JerichoGatling/2.3.0-attacker/attack.sh
@@ -2,3 +2,8 @@
 
 $GATLING_HOME/bin/gatling.sh -nr -on $(hostname) -m
 mv $GATLING_HOME/results/*/simulation.log  /opt/gatling/shared-results/$(hostname).log
+
+while [ 1 ]
+do
+    sleep 3600
+done
