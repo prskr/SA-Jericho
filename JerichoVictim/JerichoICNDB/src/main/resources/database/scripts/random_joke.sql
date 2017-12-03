f89266934ffb80a7eee561f8a3ab81181e455d34
Peter Kurfer <peter.kurfer@googlemail.com>
2017-12-03 19:58:26 +0100

    Added id to stored procedure for random jokes

--- JerichoVictim/JerichoICNDB/src/main/resources/database/scripts/random_joke.sql
+++ JerichoVictim/JerichoICNDB/src/main/resources/database/scripts/random_joke.sql
@@ -1,15 +1,19 @@
-CREATE OR REPLACE FUNCTION random_joke(OUT joke_text text, OUT category_text text)
-  AS
+CREATE OR REPLACE FUNCTION random_joke(OUT joke_id INT, OUT joke_text TEXT, OUT category_text TEXT)
+AS
 $BODY$
-  DECLARE
-  _offset int := floor(random() * (SELECT count(*) FROM jokes));
-  BEGIN
-    SELECT joke AS "joke_text", coalesce(name, '') AS "category_text"
-    FROM jokes j
+DECLARE
+  _offset INT := floor(random() * (SELECT count(*)
+                                   FROM jokes));
+BEGIN
+  SELECT
+    j.id               AS "joke_id",
+    joke               AS "joke_text",
+    coalesce(name, '') AS "category_text"
+  FROM jokes j
     LEFT JOIN categories c ON j."categoryId" = c.id
-    OFFSET _offset
-    LIMIT 1
-    INTO joke_text, category_text;
-  END;
+  OFFSET _offset
+  LIMIT 1
+  INTO joke_id, joke_text, category_text;
+END;
 $BODY$
-  LANGUAGE plpgsql VOLATILE;
+LANGUAGE plpgsql VOLATILE;
--- JerichoVictim/JerichoICNDB/src/main/resources/database/scripts/random_joke_with_filter.sql
+++ JerichoVictim/JerichoICNDB/src/main/resources/database/scripts/random_joke_with_filter.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION random_joke_with_filter(categories_filter CHARACTER VARYING [], OUT joke_text TEXT,
+CREATE OR REPLACE FUNCTION random_joke_with_filter(categories_filter CHARACTER VARYING [], OUT joke_id INT,
+                                                                                           OUT joke_text TEXT,
                                                                                            OUT category_text TEXT)
 AS
 $BODY$
@@ -7,10 +8,11 @@ DECLARE
     SELECT count(*)
     FROM jokes j
       JOIN categories cat ON j."categoryId" = cat.id
-    WHERE cat.name in (SELECT unnest(categories_filter))
+    WHERE cat.name IN (SELECT unnest(categories_filter))
   ));
 BEGIN
   SELECT
+    j.id               AS "joke_id",
     joke               AS "joke_text",
     coalesce(name, '') AS "category_text"
   FROM jokes j
@@ -18,7 +20,7 @@ BEGIN
   WHERE c.name IN (SELECT unnest(categories_filter))
   OFFSET _offset
   LIMIT 1
-  INTO joke_text, category_text;
+  INTO joke_id, joke_text, category_text;
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE;
