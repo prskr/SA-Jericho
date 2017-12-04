67420f36eb16642a04ea780227fd5973b8ae5285
Peter Kurfer <peter.kurfer@googlemail.com>
2017-12-04 23:28:13 +0100

    Fixed content type of responses

--- JerichoVictim/JerichoICNDB/src/main/kotlin/de/fhro/inf/sa/jerichoDemo/api/controllers/JokesController.kt
+++ JerichoVictim/JerichoICNDB/src/main/kotlin/de/fhro/inf/sa/jerichoDemo/api/controllers/JokesController.kt
@@ -6,6 +6,7 @@ import de.fhro.inf.sa.jerichoDemo.model.JokeDto
 import de.fhro.inf.sa.jerichoDemo.persistence.CQRSEndpoints
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
+import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.core.eventbus.EventBus
 import io.vertx.core.eventbus.Message
 import io.vertx.core.json.Json
@@ -29,7 +30,7 @@ class JokesController {
 
 			eventBus?.send<JsonObject>(CQRSEndpoints.GET_JOKE_JPA_ID, jokeId, { result ->
 				if (result.succeeded()) {
-					message.reply(result.result().body())
+					message.reply(result.result().body(), DeliveryOptions().addHeader("Content-Type", "application/json"))
 				} else {
 					manageError(message, result.cause(), "getJoke")
 				}
@@ -44,7 +45,7 @@ class JokesController {
 		try {
 			eventBus?.send<JsonObject>(CQRSEndpoints.GET_JOKES_JPA_ID, message.body(), { result ->
 				if (result.succeeded())
-					message.reply(result.result().body())
+					message.reply(result.result().body(), DeliveryOptions().addHeader("Content-Type", "application/json"))
 				else
 					manageError(message, result.cause(), "getJokes")
 			})
@@ -58,7 +59,7 @@ class JokesController {
 		try {
 			eventBus?.send<JsonObject>(CQRSEndpoints.GET_RANDOM_JOKE_JPA_ID, null, { result ->
 				if (result.succeeded()) {
-					message.reply(result.result().body())
+					message.reply(result.result().body(), DeliveryOptions().addHeader("Content-Type", "application/json"))
 				} else {
 					manageError(message, result.cause(), "getRandomJoke")
 				}
