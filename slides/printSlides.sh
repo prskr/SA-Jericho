2b59bb5c82ce291cef38e5689427a022552b5720
Thomas Mildner <thomas.mildner@yahoo.de>
2017-12-05 13:52:15 +0100

    edited runGitPitch, deleted unused scripts

--- slides/printSlides.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-decktape reveal http://localhost:9000/sINFpekurf/PTF-Exam-Slides-WS1718 ~/Downloads/PTF-Slides.pdf --no-sandbox
--- slides/runGitPitch.sh
+++ slides/runGitPitch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --rm --name gitpitch -e GP_GITLAB_BASE=https://inf-git.fh-rosenheim.de/ -e GP_GITLAB_API=https://inf-git.fh-rosenheim.de/api/v4/ -e GP_GITLAB_AS_DEFAULT=true -p 9000:9000 knsit/gitpitch
+docker run -d --rm --name gitpitch -e GP_GITHUB_AS_DEFAULT=true -p 9000:9000 knsit/gitpitch-pdf:latest
